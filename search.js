045db8492d971e698a8f2fe70beb1602017c142e
你们的小f <nmdfzf404@163.com>
2022-10-18 13:03:43 +0800

    Update search.js

--- search.js
+++ search.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const pathName = '.cache/md';
 const jsonPath = '.rdoc-dist/search.json';
+const cnamePath = '.rdoc-dist/CNAME'
 
 function readFileList(dir, filesList = []) {
   const files = fs.readdirSync(dir);
@@ -16,3 +17,4 @@ function readFileList(dir, filesList = []) {
 const filesList = readFileList(pathName);
 
 fs.writeFile(jsonPath, JSON.stringify(filesList, '', '\t'), err => err);
+fs.writeFile(cnamePath, 'note.fzf404.art', err => err);
